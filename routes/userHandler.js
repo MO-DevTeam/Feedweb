f5e6714c5d1e11fe0c715531044c44aad9ab0dc7
Mahim Agrawal <mahim23agrawal@gmail.com>
2017-08-05 23:43:16 +0530

    Bug Fixed in sign up user

--- routes/userHandler.js
+++ routes/userHandler.js
@@ -21,7 +21,7 @@ post(function (request, res) {
     userData.name = req.name;
 
     User.find({'username' : userData.username}, function (err, user) {
-            if (user !== []){
+            if (err.name === 'MongoError' && err.code === 11000){
                 res.send("Error: Username is not unique")
             }
             else{
